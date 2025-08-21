b65337a50a8036f70affd58cc72844f61d6aec7c
Kyx <kylian.colson@gmail.com>
2025-08-21 15:24:53 +0200

    feat: apply debug mode from dotenv

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import sys
+import os
+from dotenv import load_dotenv
 # Import from other folder -> Ugly
 from src.database.database import create_connection
 from src.scrapper.scrapper import scrap_poke_data
+from src.config import setup_debug_mode
 
 
 class Langs(str):
@@ -10,6 +13,12 @@ class Langs(str):
     JP = "jp"
     
 def main():
+    # Load environment variables from .env file
+    load_dotenv()
+    
+    # Setup debug mode from environment variable
+    setup_debug_mode()
+    
     connection = create_connection()
     scrap_poke_data(connection, Langs.FR)
     
